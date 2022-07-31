33a99b03d604c56ca026920c66cc916cb184f469
gsj727 <917994538@qq.com>
2022-07-31 19:57:13 +0800

    Vue demo1: tabControl

---
+++ 01_communications_demo/src/components/TabControl.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="tab-control">
+        <template v-for="(item, index) in titles" :key="item">
+            <div class="tab-control-item" @click="itemClick(index)" :class="{ active: index === currentIndex }">
+                <span>{{ item }}</span>
+
+            </div>
+        </template>
+    </div>
+</template>   
+
+<script>
+export default {
+    props: {
+        titles: {
+            type: Array,
+            default: () => []
+        }
+    },
+
+    data() {
+        return {
+            currentIndex: -1
+        }
+    },
+    emits: ["tabItemClick"],
+    methods: {
+        itemClick(index) {
+            this.currentIndex = index;
+            // console.log(index);
+            this.$emit("tabItemClick", index);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.tab-control {
+    display: flex;
+    height: 44px;
+    text-align: center;
+    align-items: center;
+}
+
+.tab-control-item {
+    flex: 1;
+    cursor: pointer;
+
+}
+
+.tab-control-item.active {
+    color: red;
+    font-weight: bold;
+}
+
+.tab-control-item.active  span {
+    padding: 8px;
+    border-bottom: 3px solid red;
+}
+</style>
