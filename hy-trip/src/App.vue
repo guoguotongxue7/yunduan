b497c104e24e1f963b46e23f59098b578ae39c99
gsj727 <917994538@qq.com>
2022-09-20 17:09:32 +0800

    hy-trip

---
+++ hy-trip/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <router-view></router-view>
+    <router-link to="/home">首页</router-link>
+    <router-link to="/message">消息</router-link>
+    <router-link to="/favor">收藏</router-link>
+    <router-link to="/order">订单</router-link>
+    
+  </div>
+</template>   
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
